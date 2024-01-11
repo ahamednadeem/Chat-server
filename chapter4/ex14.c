6e67b864970383400c8cebddd973d0b43fb33b57
ahamednadeem <ahamed@mobicip.com>
2024-01-11 15:34:16 +0530

    ex14

---
+++ chapter4/ex14.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#define swap(t, x, y) { t z; z = y; y = x; x = z; }  //z is of type t, where z is a temporary variable
+
+void main()
+{
+	int x, y;
+	x = 2;
+	y = 3;
+	printf("x = %d y = %d \n", x, y);
+	swap(int, x, y);
+	
+	printf("x = %d y = %d \n", x, y);
+}
+
