c4e8a10743a15027a843323100fbfc8fd20fedbe
ahamednadeem <ahamed@mobicip.com>
2024-01-20 12:39:14 +0530

    updated

--- chapter1/ex12.c
+++ chapter1/ex12.c
@@ -1,50 +1,34 @@
+/* program that prints its input one word per line. */
+
 #include<stdio.h>
-/*void main()
-{
-	int c;
-	while((c = getchar()) != EOF)
-	{
-		if (c == ' ')
-		{
-			printf("\n");
-		}
-		
-		else 
-		{
-			putchar(c);
-		}
-	}
-}
-*/
 
 void main()
 {
 	int c;
-	int in = 0;
-	int out = 1;
-	int state = out;
+	int IN = 1;   //Inside the word
+	int OUT = 0;  //Outside the word
+	int state = OUT;
 	while((c = getchar()) != EOF)
 	{
-		if (c == ' ' || c == '\n' || c == '\t')
-		{
-			if (state == in)
-			{
-				printf("\n");
-				state = out;
-			}
+		if(c == ' ' || c == '\n' || c == '\t')  
+		{ 
+			if(state == IN)   //checking is it is inside thw word
+				{
+					putchar('\n');
+					state = OUT;
+				}
 		}
-		else if (state == out)
+		else if(state == OUT)    //checking is it is outside thw word
 		{
-			putchar(c);
-			state = in;
+			 putchar(c);
+			 state = IN;
 		}
-		
 		else
-		{
 			putchar(c);
-		}
 	}
 }
 
 				
+				
+
 				
