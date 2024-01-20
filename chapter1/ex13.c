951f0fae17b837d64827fdd4fab580373bed97f6
ahamednadeem <ahamed@mobicip.com>
2024-01-20 16:05:43 +0530

    updated

--- chapter1/ex13.c
+++ chapter1/ex13.c
@@ -1,61 +1,73 @@
-#include<stdio.h>
+/* a program to print a histogram of the lengths of words */
+
+/*#include <stdio.h>
 
 void main()
 {
-	int maxword = 11;
-	int in = 1;
-	int out = 0;
-
-	int c, i, nc, state;
-	state = out;
-	int maxvalue;
-	int wl[maxword];
-	
-	nc = 0;
-
-	for(i = 0; i < maxword; i++)
-	{
-		wl[i] = 0;
-	}
-
+	int c;
 	while((c = getchar()) != EOF)
 	{
-		if (c == ' ' || c == '\n' || c == '\t')
-		{
-			state = out;
-			
-			if (nc > 0)
-				wl[nc]++;
-				
-			nc = 0;
-
-		}
-
-		else if(state == out)
-			{
-				state = in;
-				nc = 1;
-			}
+		if(c == ' ' || c == '\t' || c == '\n')
+			putchar('\n');
 		else
-			nc++;
-	}
-
-	maxvalue = 0;
-	for (i = 0; i < maxword; i++)
-	{
-		if (wl[i] > maxvalue)
-		{
-			maxvalue = wl[i];
-		}
-	}
-
-	for(i = 1; i < maxword; i++)
-	{
-		printf("%3d - %3d", i, wl[i]);
+			putchar('*');
 	}
 }
+			
+
+*/
+
+#include <stdio.h>
+#include<stdlib.h>
+
+#define MAXLEN  20 
+#define IN      1   // inside a word 
+#define OUT     0   // outside a word 
+
+int main()
+{
+    int c, state, i, j, counter;
+    int histogram[MAXLEN+1];
+
+    for (i = 0; i < MAXLEN; ++i)
+        histogram[i] = 0;
+    state = OUT;
+    counter = 0;
+    
+    while ((c = getchar()) != EOF)
+        if (c == ' ' || c == '\n' || c == '\t') 
+        {
+            if (state == IN && counter > 0) 
+            {
+                if (counter <= MAXLEN)
+                    ++histogram[counter-1];
+                else
+                {
+                    printf("Word limit Exceeded");
+                    exit(0);
+                }
+            }
+            
+            state = OUT;
+            counter = 0;
+        }
+        
+        else 
+        {
+            ++counter;
+            if (state == OUT)
+                state = IN;
+        }
 
 
 
-	
-	
+    for (i = 0; i < MAXLEN; ++i) 
+    {
+    	printf(" %d -->", i+1);
+        
+        for (j = 0; j < histogram[i]; ++j)
+            putchar('*');
+        putchar('\n');
+    }
+   
+}
