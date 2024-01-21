2e4adba1fb90c3a5bb680cde1300e9a29cbdb148
ahamednadeem <ahamed@mobicip.com>
2024-01-21 20:25:21 +0530

    updated

--- chapter1/ex22.c
+++ chapter1/ex22.c
@@ -1,19 +1,19 @@
+/* `fold' long input lines into two or more shorter lines */
+
 #include<stdio.h>
+
+#define lim 10
 
 void main()
 {
-	int lim = 10;
 	int character;
 	int count = 0;
-	
 	
 	while((character = getchar()) != EOF)
 	{
 		count++;
-		if (count < lim)
-		{	
+		if (count <= lim)	
 			putchar(character);
-		}
 		else
 		{
 			putchar('\n');
