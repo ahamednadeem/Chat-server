0d1bd97bcff119122c8232f3642fb5213c0d268d
ahamednadeem <ahamed@mobicip.com>
2024-01-21 19:51:45 +0530

    updated

--- chapter1/ex21.c
+++ chapter1/ex21.c
@@ -1,13 +1,17 @@
+/* program entab that replaces strings of blanks by the minimum number
+of tabs and blanks to achieve the same spacing */
+
+
 #include <stdio.h>
 
-#define SPACE_TO_TAB 8
+#define SPACE_TO_TAB 8  //default spacing
 
 int main()
 {
   int space = 0, i;
   int character;
 
-  while((character = getchar()) !=EOF) 
+  while((character = getchar()) != EOF) 
   {
     if(character == ' ') 
     {
@@ -17,14 +21,10 @@ int main()
     {
       if(space > 0) 
       {
-        for(i = 0; i < space/SPACE_TO_TAB; i++) 
-	{
+        for(i = 0; i < space/SPACE_TO_TAB; i++) //space must be greater than SPACE_TO_BAR for this loop to run
           putchar('\t');
-        }
-        for(i = 0; i< space % SPACE_TO_TAB; i++) 
-	{
+        for(i = 0; i < space % SPACE_TO_TAB; i++) 
           putchar(' ');
-        }
         space = 0;
       }
       putchar(character);
