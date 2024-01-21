fac145d9b9d43b6c7609c659f26c8202dcd9d018
ahamednadeem <ahamed@mobicip.com>
2024-01-21 16:02:53 +0530

    updated

--- chapter1/ex18.c
+++ chapter1/ex18.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
+#include<stdlib.h>
 
-#define MAXLINE 10
+#define MAXLINE 1000
 
-int getlines(char line[], int lim);
+int _getline(char line[], int lim);
 int remove_trail(char rline[]);
 
-int main() {
+int main(void) 
+{
     int len;
     char line[MAXLINE];
-
-    while ((len = getlines(line, MAXLINE)) > 0)
+    
+    while ((len = _getline(line, MAXLINE)) > 0)
         if (remove_trail(line) > 0)
-            printf("%s", line);
-
+            printf("%s\n", line);
+            
     return 0;
 }
 
-int getlines(char line[], int lim) {
-    int i, characters;
-
-    for (i = 0; i < lim - 1 && (characters = getchar()) != EOF && characters != '\n'; ++i)
-        line[i] = characters;
-    if (characters == '\n') {
-        line[i] = characters;
-        ++i;
+int _getline(char line[], int lim) 
+{
+    int i, character;
+    for (i = 0; (character = getchar()) != EOF && character != '\n'; ++i)
+    {
+    	if (i < lim - 1)
+    		line[i] = character;
+    	else
+    	{
+    		printf("Word Limit Exceeded");
+    		exit(0);
+    	}
     }
+        
+    if (character == '\n') 
+        line[i++] = character;
+        
     line[i] = '\0';
-
+    
     return i;
+
 }
 
-int remove_trail(char rline[]) {
+// To remove Trailing Blanks,tabs. Go to End and proceed backwards removing
+int remove_trail(char rline[]) 
+{
     int i;
 
-    for (i = 0; rline[i] != '\n'; ++i);
-    	--i; 
-
+    for (i = 0; rline[i] != '\n'; ++i);  //we traverse to \n
+    --i;                                                        
     for (i > 0; ((rline[i] == ' ') || (rline[i] == '\t')); --i); 
-
-    if (i >= 0) 
+    if (i >= 0)                                                 
     {
         ++i;
-        rline[i] = '\n';
-        ++i;
+        rline[i++] = '*';   //to mark the ending of the word   (for viewing purpose)
+        rline[i++] = '\n';
+        
         rline[i] = '\0';
     }
     return i;
