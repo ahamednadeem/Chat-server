4c3d829fdbaf25360ea731517be58648c6199900
ahamednadeem <ahamed@mobicip.com>
2024-02-05 13:50:04 +0530

    updated

--- chapter2/ex10.c
+++ chapter2/ex10.c
@@ -1,19 +1,19 @@
+/* converts upper case letters to lower case,
+with a conditional expression instead of if-else . */
+
+
 #include <stdio.h>
 
 int lower(int c);
 
 int main(void) 
 {
-    int c;
+	int character;
 
-    while ((c = getchar()) != EOF) {
-        putchar(lower(c));
-    }
+    	while ((character = getchar()) != EOF) 
+		putchar(lower(character));
 }
 
-int lower(int c) 
-{
-       	return c >= 'A' && c <= 'Z' ? c + 'a' - 'A' : c; 
+int lower(int c)  {
+	return c >= 'A' && c <= 'Z' ? c + 'a' - 'A' : c;  //used ternary operator 
 }
-
-
