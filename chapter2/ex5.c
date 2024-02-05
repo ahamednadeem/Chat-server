ef5922a17a36ae8364e6e81a367fef1e93bffd01
ahamednadeem <ahamed@mobicip.com>
2024-02-05 13:46:36 +0530

    updated

--- chapter2/ex5.c
+++ chapter2/ex5.c
@@ -1,7 +1,13 @@
+/* function any(s1,s2) , which returns the first location in a string s1
+where any character from the string s2 occurs, or -1 if s1 contains no characters from s2 . */
+
+
 #include <stdio.h>
+#include<stdlib.h>
+
 #define MAXLINE 1000
 
-int mgetline(char line[], int lim);
+void mgetline(char line[], int lim);
 int any(char line1[], char line2[]);
 
 int main() 
@@ -11,39 +17,42 @@ int main()
     mgetline(line1, MAXLINE);
     mgetline(line2, MAXLINE);
     res = any(line1, line2);
-    printf("%d", res);
+    printf("%d\n", res);
     	
     return 0;
 }
 
-int mgetline(char s[], int lim) 
+void mgetline(char s[], int lim)  //get input from the user
 {
     int i, character;
-    for (i = 0; i < lim - 1 && (character = getchar()) != EOF && character != '\n'; ++i)
-    {
+    for (i = 0; i < lim && (character = getchar()) != EOF && character != '\n'; ++i)
         s[i] = character;
-    }
-    if (character == '\n')
+    
+    if(i >= lim)
     {
-        s[i++] = character;
+    	printf("Line limit Exceeded \n");
+    	exit(0);
     }
+
+  
     s[i] = '\0';
 }
 
-int any(char line1[], char line2[]) 
+int any(char line1[], char line2[]) //returns the first location in a string s1 where any character from the string s2 occurs
 {
-    int i, j;
+    int i, j, flag = 0;
+
     for (j = 0; line2[j] != '\0'; ++j) 
     {
         for (i = 0; line1[i] != '\0'; ++i)
         {
         	if(line1[i] == line2[j])
         	{
-        		return i+1;
+        		return i + 1;
         	}
         }
         
-    
+  
         
     }
     return -1;
--- chapter2/ex9.c
+++ chapter2/ex9.c
@@ -1,11 +1,12 @@
+/* x &= (x-1) deletes the rightmost 1-bit
+in x . Use this observation to right a program for bitcount */
+
 #include<stdio.h>
 int bitcount(unsigned int x);
 
-
-
 void main()
 {
-	int x = 23;
+	int x = -923;
 	printf("The Number of 1's : %d \n ", bitcount(x));
 }	
 
@@ -14,7 +15,7 @@ int bitcount(unsigned int x)
     int count = 0;
     while (x != 0) 
     {
-        x &= x-1;
+        x &= (x-1);  //the rightmost one bit is deleted at each iteration
         count++;
     }
     return count;
