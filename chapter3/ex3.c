46a985fc04d900c13174f67b6e5a0dc4b200b543
ahamednadeem <ahamed@mobicip.com>
2024-02-05 14:12:28 +0530

    updated

--- chapter3/ex3.c
+++ chapter3/ex3.c
@@ -1,3 +1,6 @@
+/* function expand(s1,s2) that expands shorthand notations like a-z in
+the string s1 into the equivalent complete list abc...xyz in s2 */
+
 #include <stdio.h>
 #define MAXLINE 100
 
@@ -7,13 +10,13 @@ void expand(char s1[], char s2[]);
 int main() 
 {
     char s1[MAXLINE], s2[MAXLINE];
-    mgetline(s1, MAXLINE);
+    mgetline(s1, MAXLINE);  //user input is stored in string s1
     expand(s1, s2);
     printf("%s", s2);
     return 0;
 }
 
-int mgetline(char s[], int lim) 
+int mgetline(char s[], int lim)   //get input from the user, make sure not to give empty spaces
 {
     int i, c;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
@@ -29,7 +32,7 @@ int mgetline(char s[], int lim)
     s[i] = '\0';
 }
 
-void expand(char s1[], char s2[])
+void expand(char s1[], char s2[])  //function that expands shorthand notations 
 {
 	int i, j, c;
         i = j = 0;
