b286af5efd5f774888a273180482a83f8752c08e
ahamednadeem <ahamed@mobicip.com>
2024-02-05 14:05:25 +0530

    updated

--- chapter3/ex2.c
+++ chapter3/ex2.c
@@ -1,33 +1,34 @@
-#include<stdio.h>
+/* function escape(s,t) that converts characters like newline and tab into
+visible escape sequences like \n and \t as it copies the string t to s */
 
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXLINE 10
 
 void mgetline(char s[], int maxline);
 void escape(char s[], char t[]);
 
+
 void main()
 {
-	int maxline = 1000;
-	char s[maxline], t[maxline];
-	mgetline(t, maxline);
+	char s[MAXLINE], t[MAXLINE];
+	mgetline(t, MAXLINE);
 	escape(s, t);
 	printf("%s", s);
 }
 
-
-
-void mgetline(char t[], int lim) 
+void mgetline(char t[], int lim)  //get input line from the user
 {
     int i, character;
 
-    for (i = 0; i < lim - 1 && (character = getchar()) != EOF; ++i)
+    for (i = 0; i < lim  && (character = getchar()) != EOF; ++i)
         t[i] = character;
-
+        
     t[i] = '\0';
 }
 
-
-
-void escape(char s[], char t[])
+void escape(char s[], char t[])  //convertes newline and tabs into visible characters
 {
 	int i = 0, j = 0;
 	while (t[i] != '\0') 
@@ -47,7 +48,7 @@ void escape(char s[], char t[])
 			break;
         
 			default:
-            		s[j] = t[i];
+            		s[j] = t[i];  
             		break;
         	}
         	++i;
