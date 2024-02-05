69d252e8dcca5f2fd79565a17f42afbc4033b7d2
ahamednadeem <ahamed@mobicip.com>
2024-02-05 14:20:36 +0530

    updated

--- chapter3/ex4.c
+++ chapter3/ex4.c
@@ -1,3 +1,6 @@
+/* itoa does not handle the largest negative number, modify the code */
+
+
 #include <stdio.h>
 #include <string.h>
 #define MAXLINE 1000
@@ -10,39 +13,40 @@ int main()
 {
     int number;
     char str[MAXLINE];
-    number = -2147483648;
+    number = -2147483648; //maximum negative integer 
     itoa(number, str);
-    printf("String : %s", str);
+    printf("String : %s\n", str);
     return 0;
 }
 
-void itoa(int n, char s[]) 
+void itoa(int n, char s[])   // convertes integer to string
 {
     int i, sign;
-    sign = n;
+    sign = n; 
     i = 0;
     do 
     {
-        s[i++] = absolute(n % 10) + '0';
+        s[i++] = absolute(n % 10) + '0';   // '0' is used to convert integer to string
     } 
     while ((n /= 10) != 0);
 
     if (sign < 0)
-        s[i++] = '-';
+        s[i++] = '-';  //add the - sign atlast and then reverse it
 
     s[i] = '\0';
 
     reverse(s);
 }
 
-void reverse(char s[]) {
+void reverse(char s[])   // reverses the input array
+{
     int c, i, j;
 
     for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
         c = s[i], s[i] = s[j], s[j] = c;
 }
 
-int absolute(int k)
+int absolute(int k)  // take absolute of the input hence negative numbers won't affect the output
 {
 	k = (k > 0 ? k : -k);
 	return k;
