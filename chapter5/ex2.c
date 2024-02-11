208bc89a0556bae366fa824fa68fef88e64bca88
ahamednadeem <ahamed@mobicip.com>
2024-02-11 18:11:21 +0530

    updated

--- chapter5/ex2.c
+++ chapter5/ex2.c
@@ -1,3 +1,5 @@
+/* Write getfloat , the floating-point analog of getint */
+
 #include<stdio.h>
 #include<ctype.h>
 
@@ -8,12 +10,12 @@ char buf[BUFSIZE];
 int buffer_pointer = 0;
 
 
-int getch(void)
+int getch(void)  // get input from the buffer or from getchar() function
 {
 	return (buffer_pointer > 0) ? buf[--buffer_pointer] : getchar();
 }
 
-void ungetch(int character)
+void ungetch(int character)  // store the data in the buffer 
 {
 	if(buffer_pointer >= BUFSIZE)
 		printf("too many character\n");
@@ -22,10 +24,9 @@ void ungetch(int character)
 }
 
 
-int getfloat(float *pn)
+int getfloat(float *pn)   // get input as float data from the user and store it in the array using pointers
 {
 	int sign, character;
-	
 	float fractional = 1;
 
 	while(isspace(character = getch())); //get input until we get a non-space character
@@ -36,14 +37,12 @@ int getfloat(float *pn)
 		return -1;
 	}
 
-	sign = (character == '-') ? -1 : 1;
-
-	
+	sign = (character == '-') ? -1 : 1;  // note down the sign	
 	if(character == '+' || character == '-')
 		character = getch();
 
 	for(*pn = 0; isdigit(character); character = getch())
-		*pn = 10 * *pn + (character - '0');    //for integer part
+		*pn = 10 * *pn + (character - '0');    //for integer part, *pn is the value 
 	
 	if(character == '.')
 		character = getch();
@@ -53,11 +52,9 @@ int getfloat(float *pn)
 		*pn = 10 * *pn + (character - '0');    //for fractional part
 		fractional *= 10.0;
 	}
-	
-	
-		
+			
 	*pn *= sign;
-	*pn /= fractional;
+	*pn /= fractional;  // divide the total by fractional 
 	
 
 	if(character != EOF)
@@ -71,10 +68,10 @@ void main()
 	int i, count;
 	float array[SIZE];
 
-	for(i = 0; i < SIZE && getfloat(&array[i]) != EOF; i++)
+	for(i = 0; i < SIZE && getfloat(&array[i]) != EOF; i++)  // to get inputs from the user
 		count++;
 	
-	for(i = 0; i < count; i++)
+	for(i = 0; i < count; i++)  // to print the elements in the array
 		printf("ELEMENT AT INDEX %d : %f\n", i, array[i]);
 	
 	
