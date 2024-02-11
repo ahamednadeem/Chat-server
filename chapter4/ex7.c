6400e8f40b9951e4f3daa581978328092703f80e
ahamednadeem <ahamed@mobicip.com>
2024-02-11 11:35:25 +0530

    updated

--- chapter4/ex7.c
+++ chapter4/ex7.c
@@ -1,18 +1,17 @@
+/* Write a routine ungets(s) that will push back an entire string onto the input. */
+
 #include <stdio.h>
 #include <string.h>
 
-#define MAXBUF 100
-#define MAXLINE 100
+#define MAXBUF 1000
+#define MAXLINE 1000
 
 int bufp = 0;
 int buf[MAXBUF];
 
 int getch(void);
-
 void ungetch(int c);
-
-void ungets(char s[]);
-
+void ungets(char s[]); 
 int mgetline(char line[], int maxline);
 
 int main(void) 
@@ -28,7 +27,7 @@ int main(void)
     return 0;
 }
 
-int mgetline(char s[], int lim) 
+int mgetline(char s[], int lim)   // get input line by line from the user
 {
     int i, c;
 
@@ -41,15 +40,15 @@ int mgetline(char s[], int lim)
     s[i] = '\0';
 }
 
-void ungets(char s[]) 
+void ungets(char s[])   // pushes back the entire string to the input
 {
     int i;
     i = strlen(s);
-    while (i > 0)
-        ungetch(s[--i]);
+    while (i > 0) 
+        ungetch(s[--i]);  // now ungetch() is called and line is stored character by character in the buffer
 }
 
-void ungetch(int c) 
+void ungetch(int c)   // push the value into the buffer
 {
     if (bufp >= MAXBUF)
         printf("ungetch: too many characters\n");
@@ -57,7 +56,7 @@ void ungetch(int c)
         buf[bufp++] = c;
 }
 
-int getch(void) 
+int getch(void)   // get input from buffer or from getchar()
 { 
 	return (bufp > 0) ? buf[--bufp] : getchar(); 
 }
